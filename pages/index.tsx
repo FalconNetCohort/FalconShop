d53aad15a6ef3f5fa0bc528c559dd21a4181da20
tomisnhamilton <tomisnhamilton@gmail.com>
2023-11-18 11:13:23 -0700

    Changed title name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Listings from '../components/Listings';
+import Listings from '../components/listings';
 import RootLayout from '../components/RootLayout';
 import '../firebase';
 
 export default function Index() {
     return (
         <RootLayout>
-            <main className="flex min-h-screen flex-col items-center p-24 bg-gray-100">
+            <main className="flex min-h-screen flex-col items-center p-24 bg-gray-200">
                 <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:center">
                     <p className="mb-8 text-center border-b border-gray-300 text-white py-6 rounded-xl shadow-lg backdrop-blur-md bg-sky-700">
-                        Cadet Listings
+                        Digital Dazzle Depot
                     </p>
                 </div>
                 <Listings />
