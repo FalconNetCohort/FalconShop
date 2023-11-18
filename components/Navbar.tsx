7750a42160514c9e0ad61d61d5b3183af03be70b
Chamorgio <gio.quintan212@gmail.com>
2023-11-18 11:52:39 -0700

    Change of theme and Title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
 
 
     return (
-        <nav className="flex justify-between items-center bg-blue-500 p-4 text-white">
+        <nav className="flex justify-between items-center bg-indigo-800 p-4 text-yellow-50">
             <Link href="/" className="text-2xl font-bold">FalconShop</Link>
             <div className="space-x-4">
                 {!user ? (
