911ef6fd85311b2863db2224ffe65ba572d856bd
Mason C <masonneon@gmail.com>
2023-11-18 10:41:19 -0700

    Change Button Color
    Changes it to a different blue

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
         <RootLayout>
             <main className="flex min-h-screen flex-col items-center p-24 bg-gray-100">
                 <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:center">
-                    <p className="mb-8 text-center border-b border-gray-300 bg-gradient-to-b from-blue-500 to-blue-300 text-white py-6 rounded-xl shadow-lg backdrop-blur-md">
+                    <p className="mb-8 text-center border-b border-gray-300 text-white py-6 rounded-xl shadow-lg backdrop-blur-md bg-sky-700">
                         Cadet Listings
                     </p>
                 </div>
