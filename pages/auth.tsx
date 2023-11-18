6a5ff92d00a8f8b633429c8b1c70662a0fca5d51
tomisnhamilton <tomisnhamilton@gmail.com>
2023-11-18 13:55:19 -0700

    Fixed auth.tsx
    Fixed button prompt logic, removed redundancy

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,33 +5,14 @@ import LogIn from "@/components/LogIn"; // adjust the path accordingly
 import SignUp from "@/components/SignUp"; // adjust the path accordingly
 
 export default function Auth() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [isSignUp, setIsSignUp] = useState(false);
-    const auth = getAuth();
-
-    const handleAuth = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        try {
-            if (isSignUp) {
-                await createUserWithEmailAndPassword(auth, email, password);
-                // After successful sign-up, switch back to login view
-                setIsSignUp(false);
-            } else {
-                await signInWithEmailAndPassword(auth, email, password);
-                window.location.href = '/';
-            }
-        } catch (error: any) {
-            console.error("Error during authentication:", error.message);
-        }
-    }
 
     return (
 
     <div className="flex flex-col items-center justify-center min-h-screen text-black">
             <div>{isSignUp ? <LogIn/> : <SignUp/>}</div>
             <button onClick={() => setIsSignUp(!isSignUp)} className="mt-4 text-blue-500">
-                {isSignUp ? 'Already have an account? Log In' : 'Don’t have an account? Sign Up'}
+                {isSignUp ? 'Don’t have an account? Sign Up' : 'Already have an account? Log In'}
             </button>
         </div>
     );
