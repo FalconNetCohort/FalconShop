cfd7698b53f717b83eb5bc88ff7e1fa7a43bd9b9
tomisnhamilton <tomisnhamilton@gmail.com>
2023-11-18 13:41:55 -0700

    Initial file update
    Created LogIn and SignUp components. Changed auth page to use components.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import '../firebase'; // adjust the path accordingly
+import '../firebase';
+import LogIn from "@/components/LogIn"; // adjust the path accordingly
+import SignUp from "@/components/SignUp"; // adjust the path accordingly
 
 export default function Auth() {
     const [email, setEmail] = useState('');
@@ -25,27 +27,9 @@ export default function Auth() {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen text-black">
-            <h1 className="text-2xl mb-4">{isSignUp ? 'Sign Up' : 'Log In'}</h1>
-            <form onSubmit={handleAuth} className="w-64">
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    className="mb-2 p-2 border rounded"
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="mb-2 p-2 border rounded"
-                />
-                <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">
-                    {isSignUp ? 'Sign Up' : 'Log In'}
-                </button>
-            </form>
+
+    <div className="flex flex-col items-center justify-center min-h-screen text-black">
+            <div>{isSignUp ? <LogIn/> : <SignUp/>}</div>
             <button onClick={() => setIsSignUp(!isSignUp)} className="mt-4 text-blue-500">
                 {isSignUp ? 'Already have an account? Log In' : 'Don’t have an account? Sign Up'}
             </button>
