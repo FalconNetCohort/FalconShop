89a1c374d5cc74a986df77953beec20bfe52951e
Mason C <masonneon@gmail.com>
2024-01-10 16:02:00 -0700

    Show user email in email page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import RootLayout from '../components/RootLayout';
 import '../firebase';
 import AddCadetItem from "@/components/AddCadetItem";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function Profile() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    let umail = "jdoe@example.com"
+    
+    if(user != null && user.email != null){
+        umail = user.email
+    }
+    
     return (
         <RootLayout>
             <main className="flex min-h-screen flex-col items-center p-24 bg-gray-100">
@@ -14,8 +23,7 @@ export default function Profile() {
                         <div className="flex items-center">
                             <img src="/path-to-profile-image.jpg" alt="User profile" className="w-24 h-24 rounded-full mr-4"/>
                             <div>
-                                <p><strong>Name:</strong> John Doe</p>
-                                <p><strong>Email:</strong> john.doe@example.com</p>
+                                <p><strong>Email:</strong> {umail} </p>
                             </div>
                         </div>
                     </section>
