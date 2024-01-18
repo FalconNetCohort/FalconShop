74f8047bf6cdaee9ce2223d7441c62a20c2527c3
tomisnhamilton <tomisnhamilton@gmail.com>
2024-01-18 15:58:24 -0700

    Update Listings.tsx

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -22,34 +22,7 @@ interface SearchComponentProps {
     data: string[]; // assuming data is an array of strings
 }
 
-const SearchComponent: React.FC<SearchComponentProps> = ({ data }) => {
-    const [query, setQuery] = useState<string>(''); // State for the search query
-    const [results, setResults] = useState<string[]>([]); // State for the search results
 
-    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = event.target.value;
-        setQuery(value);
-        if (value.length > 0) {
-            const filteredData = data.filter(item =>
-                item.toLowerCase().includes(value.toLowerCase())
-            );
-            setResults(filteredData);
-        } else {
-            setResults([]);
-        }
-    };
-
-    return (
-        <div>
-            <input className="text-black" type="text" value={query} onChange={handleSearch} />
-            <ul>
-                {results.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
-        </div>
-    );
-};
 
 export default function Listings() {
 
@@ -76,13 +49,19 @@ export default function Listings() {
         };
     }, []);
     const data = items.map(item => item.title);
+    const [search, setSearch] = useState('');
+
     return (
         <section>
-            <div className="flex justify-center py-6 text-black">
-                <SearchComponent data={data} />
+            <div className="flex flex-col align-middle items-center pb-4">
+                <form className="border-4 rounded border-indigo-500">
+                    <input className="text-black" type="text" placeholder="Search for a title..." name="search" onChange={(e) => setSearch(e.target.value)}/>
+                </form>
             </div>
             <div className="mb-32 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 max-w-7xl w-full">
-                {items.map((item) => (
+                {items.filter((item) => {
+                    return search.toLowerCase() === '' ? item : item.title.toLowerCase().includes(search.toLowerCase());
+                }).map((item) => (
                     <div key={item.id} className="rounded-lg border border-gray-200 hover:border-blue-500 hover:bg-blue-100 dark:border-neutral-700 dark:bg-neutral-800/30 p-6 shadow-md hover:shadow-xl transform transition-all duration-300 hover:scale-105">
                         <h2 className="mb-3 text-xl font-semibold text-blue-600">{item.title}</h2>
                         <p className="text-sm opacity-70 mb-3">
