c4d38ca30b142245edd25e9741c1459f3463b937
tomisnhamilton <tomisnhamilton@gmail.com>
2024-01-27 09:06:04 -0700

    changed a few themes

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -108,17 +108,18 @@ export default function Profile() {
 
                             {items.map((item) => (
                                 <div key={item.id}
-                                     className="rounded-lg border border-gray-200 hover:border-blue-500 hover:bg-blue-100 dark:border-neutral-700 dark:bg-neutral-800/30 p-6 shadow-md hover:shadow-xl transform transition-all duration-300 hover:scale-105">
+                                     className="card">
                                     {currentUserId === item.createdBy && (
-                                        <p className ="block mt-2 font-bold text-red-700 mb-0">  Your Listing </p>
+                                        <p className="block font-bold text-red-700 mb-0"> Your Listing </p>
                                     )}
                                     <h2 className="card-title-font mb-3 text-xl text-blue-600">{item.title}</h2>
-                                    <p className="card-body-font opacity-70 mb-3">
+
+                                    <span className="block mt-2 font-bold text-blue-700">Price: ${item.price}</span>
+                                    <p className="card-body-font mt-3">Cadet: {item.cadetName}</p>
+                                    <p className="card-body-font my-1">Contact: {item.cadetContact}</p>
+                                    <p className="card-desc-font mb-3">
                                         {item.description}
                                     </p>
-                                    <span className="block mt-2 font-bold text-blue-700">${item.price}</span>
-                                    <p className="mt-3 text-gray-600">Cadet: {item.cadetName}</p>
-                                    <p className="mt-1 text-gray-600">Contact: {item.cadetContact}</p>
                                     <Image
                                         src={item.imageUrl}
                                         alt=""
