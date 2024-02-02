0eec1e8033d1e257aa43a8c067d59963919a1015
RajRaghulan <RajRaghulan@users.noreply.github.com>
2024-02-02 09:10:08 -0700

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
+                <title>FalconShop</title>
                 <link rel="icon" href="/assets/images/FalconShop_Icon.ico"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             </Head>
