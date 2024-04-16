1f52f19cae118792276689df16871c476cb97ca6
tomisnhamilton <tomisnhamilton@gmail.com>
2024-04-16 16:36:45 -0600

    Template for switch

--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { db } from '@/firebase';
-import { collection, getDocs, query, orderBy, where, limit, QueryConstraint } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export interface CadetItem {
@@ -17,102 +16,57 @@ export interface CadetItem {
     imageUrl: string;
 }
 
-interface ListingsProps {
+interface FilterProps {
     selectedCategories: string[];
     searchValue: string;
 }
 
-const buildQuery = (selectedCategories: string[]): QueryConstraint[] => {
-    let constraints: QueryConstraint[] = [orderBy('createdBy')];
 
-    constraints.push(limit(50));
-
-    return constraints;
-};
-
-export default function Listings({ selectedCategories, searchValue }: ListingsProps) {
-    const [items, setItems] = useState<CadetItem[]>([]);
-    const [currentUserId, setCurrentUserId] = useState<string | null>(null);
-
-
-    useEffect(() => {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setCurrentUserId(user.uid);
-            } else {
-                setCurrentUserId(null);
-            }
-        });
-    }, []);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const q = query(collection(db, 'cadetItems'), ...buildQuery(selectedCategories));
-            const querySnapshot = await getDocs(q);
-
-            let fetchedItems: CadetItem[] = querySnapshot.docs.map(doc => ({
-                ...doc.data() as CadetItem,
-                id: doc.id,
-            }));
-
-            // Client-side filtering based on category selection and search value
-            const filteredItems = fetchedItems.filter((item: CadetItem) =>
-                (!selectedCategories || selectedCategories.length === 0 || selectedCategories.includes(item.category)) &&
-                (searchValue === '' || item.title.toLowerCase().includes(searchValue.toLowerCase()))
-            );
-
-            setItems(filteredItems);
-        };
-
-        fetchData();
-    }, [selectedCategories, searchValue]);
 
     return (
-        currentUserId ?
-            <section className="flex flex-col items-center justify-center">
-                <div className="mb-32 grid mx-auto gap-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
-                    <div className="card">
-                        <h2 className="card-title-font mb-3 text-xl w-full overflow-wrap-anywhere break-words">Example</h2>
-                        <span
-                            className="block mt-2 font-bold text-blue-700 overflow-wrap-anywhere break-words">Example</span>
-                        <p className="card-body-font mt-3 text-gray-600 overflow-wrap-anywhere break-words">Cadet: Example</p>
-                        <p className="card-body-font mt-1 text-gray-600 overflow-wrap-anywhere break-words">Contact: Example</p>
-                        <p className="card-desc-font opacity-70 mb-3 overflow-wrap-anywhere break-words">Example</p>
-
-                        <Image
-                            src={"/assets/images/spark.png"}
-                            alt=""
-                            width={150}
-                            height={150}
-                            loader={({src}) => src}
-                        />
-                    </div>
-
-                    {items.map((item) => (
-                        <div key={item.id} className="card">
-                            {currentUserId === item.createdBy && (
-                                <p className="block mt-2 font-bold text-red-700 mb-0">Your Listing</p>
-                            )}
-                            <h2 className="card-title-font mb-3 text-xl w-full overflow-wrap-anywhere break-words">{item.title}</h2>
-                            <span className="block mt-2 font-bold text-blue-700 overflow-wrap-anywhere break-words">${item.price}</span>
-                            <p className="card-body-font mt-3 text-gray-600 overflow-wrap-anywhere break-words">Cadet: {item.cadetName}</p>
-                            <p className="card-body-font mt-1 text-gray-600 overflow-wrap-anywhere break-words">Contact: {item.cadetContact}</p>
-                            <p className="card-desc-font opacity-70 mb-3 overflow-wrap-anywhere break-words">{item.description}</p>
-                            <Image
-                                src={item.imageUrl}
-                                alt=""
-                                width={150}
-                                height={150}
-                                loader={({src}) => src}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </section>
-            :
-            <div className="flex justify-center h-screen">
-                <p className="text-center text-2xl text-blue-500">Please login with AFACADEMY email to view listings</p>
-            </div>
-    );
-}
+        // Bundle HTML w/ new implementation
+        // currentUserId ?
+        //     <section className="flex flex-col items-center justify-center">
+        //         <div className="mb-32 grid mx-auto gap-8 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7">
+        //             <div className="card">
+        //                 <h2 className="card-title-font mb-3 text-xl w-full overflow-wrap-anywhere break-words">Example</h2>
+        //                 <span
+        //                     className="block mt-2 font-bold text-blue-700 overflow-wrap-anywhere break-words">Example</span>
+        //                 <p className="card-body-font mt-3 text-gray-600 overflow-wrap-anywhere break-words">Cadet: Example</p>
+        //                 <p className="card-body-font mt-1 text-gray-600 overflow-wrap-anywhere break-words">Contact: Example</p>
+        //                 <p className="card-desc-font opacity-70 mb-3 overflow-wrap-anywhere break-words">Example</p>
+        //
+        //                 <Image
+        //                     src={"/assets/images/spark.png"}
+        //                     alt=""
+        //                     width={150}
+        //                     height={150}
+        //                     loader={({src}) => src}
+        //                 />
+        //             </div>
+        //
+        //             {items.map((item) => (
+        //                 <div key={item.id} className="card">
+        //                     {currentUserId === item.createdBy && (
+        //                         <p className="block mt-2 font-bold text-red-700 mb-0">Your Listing</p>
+        //                     )}
+        //                     <h2 className="card-title-font mb-3 text-xl w-full overflow-wrap-anywhere break-words">{item.title}</h2>
+        //                     <span className="block mt-2 font-bold text-blue-700 overflow-wrap-anywhere break-words">${item.price}</span>
+        //                     <p className="card-body-font mt-3 text-gray-600 overflow-wrap-anywhere break-words">Cadet: {item.cadetName}</p>
+        //                     <p className="card-body-font mt-1 text-gray-600 overflow-wrap-anywhere break-words">Contact: {item.cadetContact}</p>
+        //                     <p className="card-desc-font opacity-70 mb-3 overflow-wrap-anywhere break-words">{item.description}</p>
+        //                     <Image
+        //                         src={item.imageUrl}
+        //                         alt=""
+        //                         width={150}
+        //                         height={150}
+        //                         loader={({src}) => src}
+        //                     />
+        //                 </div>
+        //             ))}
+        //         </div>
+        //     </section>
+        //     :
+        //     <div className="flex justify-center h-screen">
+        //         <p className="text-center text-2xl text-blue-500">Please login with AFACADEMY email to view listings</p>
+        //     </div>
