16353835f77855e94a497700500a6bc5726998b4
tomisnhamilton <tomisnhamilton@gmail.com>
2024-04-17 08:57:40 -0600

    added rtdb, need migrate to rt
    Added realtime database (rtdb) to firebase.js. Need to migrate data in a proper tree

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import {getFirestore} from 'firebase/firestore';
+import { getRealtime } from 'firebase/database';
 
 
 const firebaseConfig = {
@@ -17,3 +18,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export const rtdb = getRealtime(app);
