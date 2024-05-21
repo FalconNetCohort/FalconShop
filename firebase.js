335f30b149ad1c41fafce39eb7e42008695c8239
tomisnhamilton <tomisnhamilton@gmail.com>
2024-05-21 07:43:21 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-import {getFirestore} from 'firebase/firestore'; // remove after migrate
 import { getDatabase } from "firebase/database";
 
 
