29535c0ffd966f1e849e7ac7c67bce65372f694b
Tomis Hamilton <tomisnhamilton@gmail.com>
2024-10-10 01:48:57 -0600

    rebase config

--- firebase.js
+++ firebase.js
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyCdd3VmqdWllIeiz5paGOe4h7JBWWE0ui8",
+    authDomain: "falconshop-303c4.firebaseapp.com",
+    databaseURL: "https://falconshop-303c4-default-rtdb.firebaseio.com",
+    projectId: "falconshop-303c4",
+    storageBucket: "falconshop-303c4.appspot.com",
+    messagingSenderId: "415270323891",
+    appId: "1:415270323891:web:f44f363a7743ee1f2b92c6",
+    measurementId: "G-K8CME54MV1"
 };
 
 export const app= initializeApp(firebaseConfig);
